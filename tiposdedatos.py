97bf2d4dfc921a8c667e5e7a77f95f244570e0aa
Liivii Manzano <liivii_neon@hotmail.com>
2020-04-02 12:05:24 -0600

    Primeros ejercicios

---
+++ tiposdedatos.py
@@ -0,0 +1,28 @@
+#print('Hola mundo')
+#tipos de datos
+
+entero=15
+flotantes= 5.32151
+string ='texto'
+booleano= True
+
+#operadores
+suma = 4 +5
+resta = 5-4
+multiplicacion = 5*4
+potencia = 5 ** 2
+division = 10/2
+division_exacta = 5//2
+modulo = 10 % 2 
+
+#print('El valor de la suma es : ' , suma)
+#print ('Ingresa cifras a sumar')
+print ('Ingresa cifras a multiplicar')
+numero_1 = float (input('Ingresa un numero: '))
+numero_2  = float (input('Ingresa otro numero: '))
+
+#total = numero_1 + numero_2
+total_2= numero_1 * numero_2
+
+#print('La suma es de', '{:0.3f}'.format(total))
+print('La multiplicacion  es de', '{:0.3f}'.format(total_2))

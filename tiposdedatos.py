67f7a0a170d2f1aceacbb9d744368fbfbe355431
Liivii Manzano <liivii_neon@hotmail.com>
2020-04-02 12:37:36 -0600

    tarea1

--- tiposdedatos.py
+++ tiposdedatos.py
@@ -17,12 +17,28 @@ modulo = 10 % 2
 
 #print('El valor de la suma es : ' , suma)
 #print ('Ingresa cifras a sumar')
-print ('Ingresa cifras a multiplicar')
+print ('Ingresa lo que se te pide')
 numero_1 = float (input('Ingresa un numero: '))
 numero_2  = float (input('Ingresa otro numero: '))
 
-#total = numero_1 + numero_2
-total_2= numero_1 * numero_2
+sumita = numero_1 + numero_2
+mul = numero_1 * numero_2
+res = numero_1 - numero_2
+po = numero_1 ** numero_2
+div = numero_1 / numero_2
+exac =  numero_1 // numero_2
+mod = numero_1 % numero_2
 
-#print('La suma es de', '{:0.3f}'.format(total))
-print('La multiplicacion  es de', '{:0.3f}'.format(total_2))
+print('RESULTADOS')
+
+print('La suma es de', '{:0.3f}'.format(sumita))
+print('La multiplicacion  es de', '{:0.3f}'.format(mul))
+print('La resta es de', '{:0.3f}'.format(res))
+print('La potencia es de', '{:0.3f}'.format(po))
+print('La division es de', '{:0.3f}'.format(div))
+print('La division exacta es de', '{:0.3f}'.format(exac))
+print('El modulo  es de', '{:0.3f}'.format(div))
+
+
+
+

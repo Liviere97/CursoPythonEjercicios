8f65d15372055758eff9caeda76c689b9a5a39e3
Liivii Manzano <liivii_neon@hotmail.com>
2020-04-03 11:03:26 -0600

    ejercicios 5 clase

---
+++ iva.py
@@ -0,0 +1,7 @@
+#Programa paracalcular el iva
+
+iva= 0.16
+print ('El valor del iva es', iva)
+precio = float(input ('ingresa el precio del producto'))
+total = precio + iva
+print ('El costo total es de: ', total)
